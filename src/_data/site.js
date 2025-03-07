60559de8a12a42eb4cbbc3404b9d56052e811208
Denise <denisenadal@outlook.com>
2025-03-06 21:09:45 -0500

    basic template set up

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,6 +6,7 @@ const routes = {
 }
 const baseUrl = routes[process.env.ELEVENTY_ENV]
 
+
 module.exports =  {
     title: "Denise Nadal",
     subtitle: "Product & Design",
@@ -14,9 +15,10 @@ module.exports =  {
     metaImage: "images/unicorn-06.jpg",
     bioImage: "images/denise.webp",
     colors:  ["pink","red","orange","yellow","green","blue","purple"],
-    metaDesc: "Denise Nadal's Product & Web Portfolio",
+    desc: "Denise Nadal's Product & Web Portfolio",
     keywords: "UI/UX Design, Web Design, Front-End Development, Front-End Engineering,Design Portfolio, Development Portfolio, Engineering Portfolio, Design Wizard, Design Unicorn",
     baseURL: baseUrl,
-    isDev: isDev
+    isDev: isDev,
+    GTM_ID: process.env.GTM_ID
 
 }
