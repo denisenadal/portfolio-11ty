1d796d2a5cf77960fda425287a9edd059af6e33c
Denise <denisenadal@outlook.com>
2025-09-11 22:17:04 -0400

    ready to review

---
+++ src/_data/experience.js
@@ -0,0 +1,125 @@
+const exp = 
+[{
+	"timeperiod": "Feb 2025 - current",
+	"company": "Freelance",
+	"title": "Web Designer & Developer",
+	"website": "https://denisenadal.com",
+	"location": "Baltimore, MD",
+	"description": "Design and development of websites and SaaS platforms for clients of all sizes. I specialize in building small, lightweight, and responsive websites, and complex and flexible WordPress installations, including theme, widget, and plugin development. ",
+	"highlights": [
+	],
+	"sample": "elp-kansas"
+},{
+	"timeperiod": "Mar 2024 - Feb 2025",
+	"company": "EcoMap Technologies",
+	"title": "Director of Product Design",
+	"website": "https://ecomap.tech",
+	"location": "Baltimore, MD",
+	"description": "Coached and mentored a team of designers, collaborated with engineers, product managers, sales, and support, and acted as voice of the customer. As a detail-oriented leader, I enjoyed building cross-functional relationships with stakeholders and finding innovative solutions to challenging problems.",
+	"highlights": ["Led customer discovery and backlog prioritization.","Developed processes to validate, build and test solutions.","Product manager for Pathways, a new feature which doubled engagement time, tripled user retention, and nominated for Product of the Year 2024 by Technical.ly","Led effort to create a new visual brand identity."
+	],
+	"sample": "ecomap-pathways"
+},{
+	"timeperiod": "Aug 2022 - Mar 2024",
+	"company": "EcoMap Technologies",
+	"title": "Design Engineer",
+	"website": "https://ecomap.tech",
+	"location": "Baltimore, MD",
+	"description": "First UI/UX hire; pioneered our UX design and research processes and spearheaded the design of the Ecomap platform to optimize for usability.",
+	"highlights": ["Modernized app UI for the Ecomap platform, improving usability.", "Design and front-end development for our AI-powered Ecosystem Chatbot", "Developed Figma Design Libraries and components to match our design system."],
+	"sample": "ecomap-submaps"
+},{
+		"timeperiod": "Aug 2020 - Aug 2022",
+		"company": "FIFCO USA",
+		"title": "Web Designer & Developer",
+		"website": "https://www.fifcousa.com",
+		"location": "Remote (Rochester, NY)",
+		"description": "Lead designer and developer for a suite of brand and corporate websites. Rapidly designing, building, testing, and launching multiple web-based projects concurrently. Managing team project backlog, and prioritization.",
+		"highlights": [
+			"Designed and developed company intranet on WordPress.",
+			"Design and development for a holiday marketplace featuring black-owned businesses, that increased site traffic by 20%."
+		],
+	"sample": "fifco-intranet"
+	},
+	{
+		"timeperiod": "Sept 2019 - March 2020",
+		"company": "Pointman",
+		"title": "UX Design & Product Management",
+		"website": "https://www.pointman.com",
+		"location": "Buffalo, NY",
+		"description": "Oversaw the entire design process, from research, ideation, design and testing for desktop and mobile apps for HVAC contractors. Created user personas, customer journey maps, workflow diagrams. Assisted Product Manager with managing product backlog, writing user stories, tracking product analytics and user metrics.",
+		"highlights": [
+			"Facilitated customer discovery, design workshops, & lean experiments.",
+			"Design new mobile-first UI and user workflows for the Pointman app"
+		],
+	"sample": "pm-home-maintenance"
+	},
+	{
+		"timeperiod": "May 2018 - Sept 2019 ",
+		"company": "Pointman",
+		"title": "UX Design & Front-End Engineering",
+		"website": "https://www.pointman.com",
+		"location":  "Buffalo, NY",
+		"description": "Front End Engineering in AngularJS and UI/UX Design for the Pointman app, a suite of field management software and mobile app for HVAC contractors.",
+		"highlights": [
+			"Facilitated cross-functional, collaborative design & research processes",
+			"Developed a Sketch UI library to accelerate the speed of creating prototypes. "
+		],
+	"sample": "pm-iq"
+	},
+	{
+		"timeperiod": "Dec 2016 - April 2018",
+		"company": "Utah Tech University",
+		"title": "Web Developer",
+		"website": "https://dixie.edu",
+		"location": "St George, UT",
+		"description": "Primary developer and team lead for 100+ University departmental and promotional websites. Designed and developed new themes and templates in WordPress.",
+		"highlights": [
+			"Used UX research to develop a new students page that increased traffic by 50%.",
+			"Acted as project manager: coordinating projects, managing deadlines, resources, resolving bugs, collecting user feedback, etc."
+		],
+	"sample": "dixie-portals"
+	},
+	{
+		"timeperiod": "Sept 2015 - Dec 2016 ",
+		"company": "Utah Tech University",
+		"title": "Head Intern, Web Content Design",
+		"website": "https://dixie.edu",
+		"location": "St George, UT",
+		"description": "Lead & mentor team of interns in designing, developing and maintaining 100+ University departmental and promotional websites and related content.",
+		"highlights": [
+			"Migrated over 10,000 pages into WordPress within 6 months",
+			"Facilitated cross-functional, collaborative design & research processes",
+			"Acted as project manager: coordinating projects and communicating with clients."
+		],
+	"sample": false
+	}
+]
+const stats = [
+    {
+        "id": "pm",
+        "num": 2,
+        "field": "Product Management",
+        "desc": "I’m always learning new things, and my natural love of organization, planning and scheming for the future naturally led me to Product Management. I’m endlessly curious about problems and how to best solve them."
+    },
+    {
+        "id": "web",
+        "num": "10+",
+        "field": "Web Development",
+        "desc": "My first website was on geocities! Web Design was just another creative outlet for me until I started my professional career in 2016. While I’ve learned frameworks like Angular, React and Vue, I am happiest in in plain HTML, CSS and JS."
+    },
+    {
+        "id": "design",
+        "num": "8+",
+        "field": "Product & UX Design",
+        "desc": "I’ve been a designer my whole life. As a teen I designed books, zines, comics and more. I got a degree in Illustration in 2011 and learned how to craft solutions for clients. I’ve shifted to crafting experiences and interfaces in 2016, but the process stays the same."
+    }
+]
+
+module.exports =   function (configData) {
+	// exp.forEach(e =>{
+	// 	sampleWork = 
+	// })
+	return exp
+}
+
